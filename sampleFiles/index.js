869991557a5cb328fa785a3848eafc8c65018518
Jorge Fuentes <jsjfuentesj@yahoo.com>
2017-10-21 16:23:02 -0700

    Add boilerplate flask

---
+++ sampleFiles/index.js
@@ -0,0 +1,8 @@
+ReactDOM.render(
+  <h1 id='title'
+  className='header'
+  style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
+  Hello World	
+  </h1>,
+  document.getElementById('react-container')
+)
