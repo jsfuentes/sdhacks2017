2e82927eb3157b59572b55bfa3245eb9ee4a9e9a
Amit Saxena <send.amit@yahoo.com>
2017-10-21 21:06:43 -0700

    Added comment

--- insertingdb.py
+++ insertingdb.py
@@ -1,6 +1,6 @@
-
-
-#prefex's in the the file that testbs.py generates
+#NOTE: this file was never used
+ 
+#constants that hold the prefex's in the the file that testbs.py generates
 WORD_STR = "word: "
 W_LEN = len(WORD_STR) 
 LINK_STR = "link: "
@@ -11,7 +11,13 @@ file = open('link_record.txt', 'w')
 elementList = file.readLines()
 
 for i in range(0,len(elementList)):
+    w_index = elementList[i].index(WORD_STR)
+    link_index = elementList[i].index(LINK_STR)
+
+    word = elementList[i][w_index+W_LEN]
+
     
+    link = elementList[i][]
 
 
 
